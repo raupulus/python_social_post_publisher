e6d4b7b5b856f8bd0a32230c180a91c7ba39bbc1
Raúl Caro Pastorino <raul@fryntiz.dev>
2025-07-06 20:34:38 +0200

    Preparing base for deployment

---
+++ deploy/config.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+## Este script genera el archivo .env
+
+################ Creo variables de trabajo ################
+
+WORKSCRIPT="$(pwd)"
+
+################ Comprueba requisitos ################
+
+## En caso de ser root se aborta.
+if [[ $(whoami) = 'root' ]]; then
+    echo 'No puedes ejecutar este script como root'
+    exit 1
+fi
+
+## En caso de no encontrar archivos esenciales para la ejcución se aborta.
+if [[ ! -f "${WORKSCRIPT}/.env.example" ]] ||
+   [[ ! -d "${WORKSCRIPT}/deploy" ]] ||
+   [[ ! -f "${WORKSCRIPT}/deploy/functions.sh" ]]; then
+    echo 'Este script solo puede ser ejecutado desde la raíz del proyecto.'
+    exit 1
+fi
+
+## En caso de existir un archivo .env se omiten operaciones.
+if [[ -f "${WORKSCRIPT}/.env" ]]; then
+    echo 'Ya existe un archivo .env en el proyecto, omitiendo operaciones.'
+    exit 0
+fi
+
+################ Incluyo archivos de funciones ################
+
+source "${WORKSCRIPT}/deploy/functions.sh"
+
+################ Comienza el flujo de generar .env ################
+
+## Creo el archivo .env a partir del archivo con parámetros predefinidos.
+
+#cp "${WORKSCRIPT}/.env.example" "${WORKSCRIPT}/.env"
+#chmod ug+rw "${WORKSCRIPT}/.env"
+
+## General
+
+#replace_or_add_var_in_file "${WORKSCRIPT}/.env" 'varname???' "${varname???}"
+
+echo ""
+#echo "Resultado del .env final:"
+#cat "${WORKSCRIPT}/.env"
+
+## Enlazo almacenamiento con los perfiles
+link_data_storage "${WORKSCRIPT}"
+
+exit 0
---
+++ deploy/functions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+##
+## Comprueba si existe una variable=valor en un archivo o la añade.
+## La comprobación se hace: Comienza por el nombre de la variable hasta
+## encontrar un "=", por ejemplo "mi_variable="
+##
+replace_or_add_var_in_file() {
+    local FILE="${1}"
+    local VAR="${2}"
+    local VALUE_RAW="${3}"
+
+    VALUE=$(echo ${VALUE_RAW} | sed 's/\//\\\//g')
+
+    ## Almaceno la línea compuesta de variable=valor.
+    local LINE="${VAR}=${VALUE}"
+
+    local REGEXP="s/^.*${VAR}\s*=.*$/${LINE}/"
+
+    echo "La línea completa quedará así: ${LINE}"
+    echo "La expresión regular quedará así: ${REGEXP}"
+
+    sed -r -i "${REGEXP}" "${FILE}"
+}
+
+##
+## Enlaza el almacenamiento estático de perfiles en el sistema de archivos
+##
+link_data_storage() {
+    local DESTINY='/var/www/storage/app_python_social_post_publisher'
+    local SCRIPT_PATH="${1}"
+
+    ln -s "${SCRIPT_PATH}/data" '/var/www/storage/app_python_social_post_publisher'
+}
