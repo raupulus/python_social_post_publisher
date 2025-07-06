de86e651cd2d3d8f01489d704fd3bdd7e1336421
Raúl Caro Pastorino <raul@fryntiz.dev>
2025-07-07 00:25:10 +0200

    Forced env at change

--- app/social_networks/__init__.py
+++ app/social_networks/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -9,68 +9,70 @@ from abc import ABC, abstractmethod
 import os
 from dotenv import load_dotenv
 
+
 class SocialNetwork(ABC):
     """
     Clase base abstracta para todas las redes sociales.
     Define la interfaz común que deben implementar todas las redes sociales.
     Así simplifico el sistema para publicar en cada red social.
     """
-    
-    def __init__(self):
+
+    def __init__ (self):
         """
         Inicializo la red social.
         """
         self.name = self.__class__.__name__
-    
-    def load_config(self, project):
+
+    def load_config (self, project):
         """
         Cargo la configuración de la red social desde el archivo .env del
         proyecto.
-        
+
         Args:
             project (str): Nombre del proyecto (Debe corresponder con el nombre del archivo .env)
         """
         env_file = os.path.join('data', 'profiles', f'{project}.env')
-        load_dotenv(env_file)
-    
+        load_dotenv(env_file, override=True)
+
     @abstractmethod
-    def publish(self, content, title=None, hashtags=None, project=None, images=None):
+    def publish (self, content, title=None, hashtags=None, project=None,
+                 images=None):
         """
         Publica contenido en la red social.
-        
+
         Args:
             content (str): Contenido a publicar
             project (str): Nombre del proyecto (Debe corresponder con el nombre del archivo .env)
             title (str, optional): Título del contenido
             hashtags (list, optional): Lista de hashtags
             images (list, optional): Lista de rutas a imágenes
-            
+
         Returns:
             dict: Resultado de la publicación
         """
         pass
-    
-    def format_content(self, content, title=None, hashtags=None):
+
+    def format_content (self, content, title=None, hashtags=None):
         """
         Formatea el contenido para la publicación.
-        
+
         Args:
             content (str): Contenido a publicar
             title (str, optional): Título del contenido
             hashtags (list, optional): Lista de hashtags
-            
+
         Returns:
             str: Contenido formateado
         """
         formatted_content = content
-        
+
         # Añado título si existe
         if title:
             formatted_content = f"{title}\n\n{formatted_content}"
-        
+
         # Añado hashtags si existen (convierto "tag" en "#tag" de lo recibido)
         if hashtags and len(hashtags) > 0:
-            hashtags_text = ' '.join(hashtags)
+            hashtags_text = ' '.join([f"#{tag}" for tag in hashtags])
             formatted_content = f"{formatted_content}\n\n{hashtags_text}"
-        
+
         return formatted_content
